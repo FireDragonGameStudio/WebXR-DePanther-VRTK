8f54b2cd93517c5e58199a8e3c9aa7e54d12ce1a
Max <FireDragonGameStudio@users.noreply.github.com>
2023-03-03 11:15:56 +0100

    adding everything for gh pages build

--- Assets/Scripts/InputTextModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using Zinnia.Action;
-
-[Serializable]
-public class InputTextModel {
-    public string Name;
-    public BooleanAction BooleanInput;
-}
--- Assets/Scripts/Vector2DAxisModel.cs
+++
@@ -1,3 +0,0 @@
-public enum Vector2DAxisModel {
-    X = 0, Y = 1,
-}
